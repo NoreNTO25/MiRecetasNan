d4e63bdc97ee502e42450e93f4a3f886e6ac2a8f
nancy <nancynore.tapiao@gmail.com>
2026-08-12 14:58:41 -0500

    "Implementar lista de recetas y ViewModel"

--- app/src/main/java/com/example/mirecetasnan/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/mirecetasnan/ui/navigation/NavGraph.kt
@@ -4,23 +4,26 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.mirecetasnan.ui.screens.DetalleRecetaScreen
 import com.example.mirecetasnan.ui.screens.AjustesScreen
+import com.example.mirecetasnan.ui.screens.DetalleRecetaScreen
 import com.example.mirecetasnan.ui.screens.ListaRecetasScreen
-
 
 @Composable
 fun NavGraph() {
     val navController = rememberNavController()
+
     NavHost(
-        navController = navController, startDestination = "lista"
+        navController = navController,
+        startDestination = "lista"
     ) {
         composable("lista") {
-            `ListaRecetasScreen`()
+            ListaRecetasScreen()
         }
+
         composable("detalle") {
             DetalleRecetaScreen()
         }
+
         composable("ajustes") {
             AjustesScreen()
         }
--- app/src/main/java/com/example/mirecetasnan/viewmodel/RecetaViewModel.kt
+++ app/src/main/java/com/example/mirecetasnan/viewmodel/RecetaViewModel.kt
@@ -1,15 +1,17 @@
 package com.example.mirecetasnan.viewmodel
+
 import androidx.lifecycle.ViewModel
 import com.example.mirecetasnan.data.Receta
 
-class RecetaViewModel : ViewModel(){
+class RecetaViewModel : ViewModel() {
+
     val recetas = listOf(
         Receta(
             id = 1,
             nombre = "Arroz con pollo",
-            descripcion = "Recetas popular",
-            ingredientes = "Arroz,pollo,verduras, salsa china sal al gusto ",
-            preparacion = "Cocinar todos los ingredientes,arroz,pollo, refreir las verduras con un poco de salsa china ",
-    )
+            descripcion = "Receta popular",
+            ingredientes = "Arroz, pollo, verduras, salsa china, sal al gusto",
+            preparacion = "Cocinar todos los ingredientes, arroz y pollo. Sofreír las verduras con un poco de salsa china."
+        )
     )
 }
